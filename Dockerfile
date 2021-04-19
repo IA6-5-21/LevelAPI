dd6a36eda3328b3ae74a2d6d69860cdcd6611f47
Jørgen Sneisen <jorgen.sneisen@lf-nett.no>
2021-04-19 21:01:49 +0200

    Finish the traditional.py file

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM fastdotai/fastai:latest
 
-RUN apt-get update && apt-get install -y git python3-dev gcc \
+RUN apt-get update && apt-get install -y python3-opencv git python3-dev gcc \
     && rm -rf /var/lib/apt/lists/*
 
 COPY requirements.txt .
--- app/ApiHandling.py
+++ app/ApiHandling.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from fastai.vision.all import *
+#from fastai.vision.all import *
 import traditional as trad
 import uvicorn
 import asyncio
@@ -11,7 +11,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
+#app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
 origins = [
@@ -30,16 +30,16 @@ app.add_middleware(
 @app.on_event("startup")
 async def startup_event():
     """Setup the learner on server start"""
-    global learn
-    loop = asyncio.get_event_loop()  # get event loop
-    tasks = [asyncio.ensure_future(setup_learner())]  # assign some task
-    learn = (await asyncio.gather(*tasks))[0]  # get tasks
+    #global learn
+    #loop = asyncio.get_event_loop()  # get event loop
+    #tasks = [asyncio.ensure_future(setup_learner())]  # assign some task
+    #learn = (await asyncio.gather(*tasks))[0]  # get tasks
 
 
 @app.post("/fastai/predict")
 async def machineLearningPrediction(file: bytes = File(...)):
-    pred = learn.predict(file)
-    return {"result": pred[0]}
+    #pred = learn.predict(file)
+    return {"result": "pred[0]"}
 
 
 class Item(BaseModel):
@@ -51,7 +51,8 @@ class Item(BaseModel):
 async def traditionalPrediction(item: Item):
     #call something like:
     prediction = trad.predict(item.image)
-    return {"level": prediction["level"]}
+    return prediction
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=5000)
+#    uvicorn.run(app, host="localhost", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
